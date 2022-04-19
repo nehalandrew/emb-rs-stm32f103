e8e237206ffe5d6875d5f52486f35f54573b7878
Andrew <rtpdfv@yahoo.com>
2022-04-19 21:35:45 +0200

    .

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-openocd -f interface/stlink-v2-1.cfg -f target/stm32f1x.cfg
+#! /bin/bash
 
-gdb-multiarch -q -ex "target remote :3333" ./target/thumbv7m-none-eabi/debug/stm32
+# Open On-Chip Debugger
+openocd -f interface/stlink-v2-1.cfg -f target/stm32f1x.cfg &
+
+# GNU DeBugger
+gdb-multiarch -q -ex "target remote :3333" ./target/thumbv7m-none-eabi/debug/stm32;
