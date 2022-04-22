229ad23673a46be3e4ea55453695df3524186df1
Andrew <rtpdfv@yahoo.com>
2022-04-22 11:23:47 +0200

    Butoon and flags.

--- src/main.rs
+++ src/main.rs
@@ -9,48 +9,56 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
-
-use nb::block;
-
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use panic_halt as _;
+use stm32f1xx_hal::{gpio::PinState, pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    let x = 123;
     // Get access to the core peripherals from the cortex-m crate
     let cp = cortex_m::Peripherals::take().unwrap();
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding HAL structs
     let mut flash = dp.FLASH.constrain();
     let rcc = dp.RCC.constrain();
 
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in `clocks`
+    let clock = rcc.cfgr.freeze(&mut flash.acr);
     // Acquire the GPIOC peripheral
+    let mut gpioa = dp.GPIOA.split();
+    // let mut gpiob = dp.GPIOB.split();
     let mut gpioc = dp.GPIOC.split();
 
     // Configure gpio C pin 13 as a push-pull openocd -f interface/stlink-v2-1.cfg -f target/stm32f1x.cfgoutput. The `crh` register is passed to the function
     // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut led = gpioc
+        .pc13
+        .into_push_pull_output_with_state(&mut gpioc.crh, PinState::High);
+    // COnfigure button
+    let button = gpioa.pa0.into_pull_up_input(&mut gpioa.crl);
+
+    // Configure the states
+    // let mut led_state = false;into_push_pull_output(&mut gpioc.crh);
+    let mut button_up = false;
+
     // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(1.Hz()).unwrap();
-    let a = 123;
     // Wait for the timer to trigger an update and change the state of the LED
+    let mut delay = cp.SYST.delay(&clock);
+
     loop {
-        block!(timer.wait()).unwrap();
-        let a = 123;
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        let a = 123;
-        led.set_low();
-        let a = 123;
+        let button_state = button.is_low();
+        if button_up && button_state {
+            button_up = false;
+            delay.delay_ms(10u8);
+            match button_state {
+                true => led.toggle(),
+                _ => (),
+            }
+        } else if !button_state {
+            button_up = true;
+            delay.delay_ms(10u8);
+        }
     }
 }
