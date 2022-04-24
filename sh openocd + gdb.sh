522802454387ab51b558237ce0db2664e3fe2512
Andrew <rtpdfv@yahoo.com>
2022-04-25 00:20:47 +0200

    Semihosting

--- sh openocd + gdb.sh
+++ sh openocd + gdb.sh
@@ -6,4 +6,10 @@ openocd -f interface/stlink-v2-1.cfg -f target/stm32f1x.cfg &
 # GNU DeBugger
 gdb-multiarch -q -ex "target remote :3333" ./target/thumbv7m-none-eabi/debug/stm32;
 
+load;
+
 layout split;
+
+monitor arm semihosting true;
+
+continue;
---
+++ src/semihosting.rs
@@ -0,0 +1,19 @@
+#![allow(clippy::empty_loop)]
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use panic_semihosting as _;
+
+use cortex_m_semihosting::hprintln;
+use stm32f1xx_hal as _;
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Hello, world!");
+    loop {
+        hprintln!("Hello, world!");
+    }
+}
