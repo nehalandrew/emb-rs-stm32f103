c75cc24e2013360b754bcb48bc49a3dd73ed0524
Andrew <rtpdfv@yahoo.com>
2022-04-26 23:31:43 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@
 #![no_main]
 #![no_std]
 
-use panic_halt as _;
+// use alloc::format;
+// use panic_halt as _;
+use panic_semihosting as _;
 
 use cortex_m::asm;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
-
 use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+
+use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
 
 #[entry]
 fn main() -> ! {
@@ -26,18 +29,22 @@ fn main() -> ! {
     // let (_pa15, _pb3, pb4) = afio.mapr.disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
 
     // TIM4
-    let pb6 = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
-    let pb7 = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
+    let pb6 = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+    let pb7 = gpiob.pb7.into_alternate_push_pull(&mut gpiob.crl);
 
     let pwm = Timer::new(p.TIM4, &clocks).pwm_hz((pb6, pb7), &mut afio.mapr, 50.Hz());
 
-    let max = pwm.get_max_duty();
+    let maxd = pwm.get_max_duty(); // 53333
+    let min = pwm.get_max_duty() / 31; // 1720
+    let max = pwm.get_max_duty() / 9; // 5925
+
+    // hprintln!("max duty: {}", maxd);
+    // hprintln!("max duty: {}", min);
+    // hprintln!("max duty: {}", max);
 
     let mut pwm_channels = pwm.split();
 
     // Enable the individual channels
-    pwm_channels.0.enable();
-    pwm_channels.1.enable();
 
     // full
     // dim
@@ -46,17 +53,26 @@ fn main() -> ! {
     // asm::bkpt();
 
     // zero
-    // pwm_channels.0.set_duty(0);
-    // pwm_channels.1.set_duty(0);
+    pwm_channels.0.set_duty(min);
+    pwm_channels.1.set_duty(min);
 
+    pwm_channels.0.enable();
+    pwm_channels.1.enable();
     // asm::bkpt();
     let mut delay = cp.SYST.delay(&clocks);
+    delay.delay_ms(100u16);
     loop {
-        pwm_channels.0.set_duty(max / 2);
-        pwm_channels.1.set_duty(max / 2);
-        delay.delay_ms(1000u16);
-        pwm_channels.0.set_duty(0);
-        pwm_channels.1.set_duty(0);
-        delay.delay_ms(1000u16);
+        // for i in min..=max {
+        pwm_channels.0.set_duty(max);
+        pwm_channels.1.set_duty(max);
+        delay.delay_ms(5000u16);
+        // }
+        pwm_channels.0.set_duty(min);
+        pwm_channels.1.set_duty(min);
+        delay.delay_ms(5000u16);
+        // delay.delay_ms(1000u16);
+        // pwm_channels.0.set_duty(0);
+        // pwm_channels.1.set_duty(0);
+        // delay.delay_ms(1000u16);
     }
 }
